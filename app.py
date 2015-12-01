635795642d1cac593de6f92cda929fb526b3a2ba
wencanluo <wencanluo.cn@gmail.com>
2015-11-30 21:49:31 -0500

    GET Tasks

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+#!flask/bin/python
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+from flask import jsonify
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route("/qualityprediction", methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
