4130e651aec5e80ecc60ee1c210bafa8b53ed303
wencanluo <wencanluo.cn@gmail.com>
2016-03-21 21:34:49 -0400

    Add evaluate initial code

---
+++ src/evaluate.py
@@ -0,0 +1,13 @@
+def evaluate(course):
+    pass
+    
+
+if __name__ == '__main__':
+    
+    import ConfigParser
+    import sys
+    
+    config = ConfigParser.RawConfigParser()
+    config.read('../config/default.cfg')
+    
+    
